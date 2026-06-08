20c48b7e9df5d05c95a8add8a42e2971a10a1613
Navya-git-account <bommishettinavya@gmail.com>
2026-06-07 21:22:11 -0500

    Complete advanced SQL queries practice

---
+++ advanced-sql-practice.sql
@@ -0,0 +1,89 @@
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE customers (
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ first_name VARCHAR(50),
+ last_name VARCHAR(50)
+);
+
+CREATE TABLE orders (
+ id INT PRIMARY KEY,
+ customer_id INT NULL,
+ order_date DATE,
+ total_amount DECIMAL(10, 2),
+ FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+INSERT INTO customers (id, first_name, last_name) VALUES
+(1, 'John', 'Doe'),
+(2, 'Jane', 'Smith'),
+(3, 'Alice', 'Smith'),
+(4, 'Bob', 'Brown');
+
+INSERT INTO orders (id, customer_id, order_date, total_amount) VALUES
+(1, 1, '2023-01-01', 100.00),
+(2, 1, '2023-02-01', 150.00),
+(3, 2, '2023-01-01', 200.00),
+(4, 3, '2023-04-01', 250.00),
+(5, 3, '2023-04-01', 300.00),
+(6, NULL, '2023-04-01', 100.00);
+
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+--  TASK 2: Write GROUP BY queries with aggregate functions like SUM and COUNT 
+SELECT customer_id, SUM(total_amount) AS total_spent
+FROM orders
+GROUP BY customer_id;
+
+SELECT customer_id, order_date, SUM(total_amount) AS total_spent
+FROM orders
+GROUP BY customer_id, order_date;
+
+SELECT customer_id, SUM(total_amount) AS total_spent
+FROM orders
+WHERE total_amount > 200
+GROUP BY customer_id;
+
+-- Task 3: Apply WHERE and HAVING clauses to filter data 
+
+-- Task: Filter grouped results using HAVING
+SELECT customer_id, SUM(total_amount) AS total_spent
+FROM orders
+GROUP BY customer_id
+HAVING SUM(total_amount) > 200;
+
+-- Task: Use WHERE and HAVING together
+SELECT customer_id, SUM(total_amount) AS total_spent
+FROM orders
+WHERE total_amount > 100
+GROUP BY customer_id
+HAVING SUM(total_amount) > 250;
+
+
+-- Task1: Use JOIN statements to combine data from multiple tables 
+-- INNER JOIN - Display all orders with customer information
+SELECT orders.id, customers.first_name, customers.last_name, orders.order_date, orders.total_amount
+FROM orders
+INNER JOIN customers ON orders.customer_id=customers.id;
+
+-- LEFT JOIN - Display all orders, including orders without a customer
+SELECT orders.id, customers.first_name, customers.last_name, orders.order_date, orders.total_amount
+FROM orders
+LEFT JOIN customers ON orders.customer_id=customers.id;
+
+-- RIGHT JOIN - Display all customers, including those without orders
+SELECT orders.id, customers.first_name, customers.last_name, orders.order_date, orders.total_amount
+FROM orders
+RIGHT JOIN customers ON orders.customer_id=customers.id;
+
+-- task 4: Practice using SubQueries to create dynamic filters 
+-- Subquery
+SELECT *
+FROM orders
+WHERE total_amount >
+(
+    SELECT AVG(total_amount)
+    FROM orders
+);
